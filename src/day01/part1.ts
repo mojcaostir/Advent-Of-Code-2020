40e7967b8a47d60299676017df5e04b7eb8590e2
Mojca <mojca.ostir@gamanza.com>
2020-12-06 17:57:47 +0100

    day 1: removed double log, functionalized the code

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -1,12 +1,17 @@
 import {readFileSplitByNewLine} from "../util";
 
-const data = readFileSplitByNewLine('/day01/input.txt').map(Number);
-
-for (const number of data) {
-  for (let i = 0; i < data.length; i++) {
-    if (number + data[i] == 2020) {
-      const res: number = number * data[i]
-      console.log(res)
+function multiplyTwoThatSumTo2020(data: number[]): number | undefined {
+  for (const number of data) {
+    for (let i = 0; i < data.length; i++) {
+      if (number + data[i] == 2020) {
+        return number * data[i]
+      }
     }
   }
 }
+
+const data = readFileSplitByNewLine('/day01/input.txt').map(Number);
+
+const day01part1Result = multiplyTwoThatSumTo2020(data);
+
+console.log('Multiplied two expenses that sum up to 2020: ' + day01part1Result);
--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,17 +1,22 @@
 import {readFileSplitByNewLine} from "../util";
 
-const data = readFileSplitByNewLine('/day01/input.txt').map(Number);
-
-for (let k = 0; k < data.length; k++) {
-  const number1 = data[k]
-  for (let i = k+1; i < data.length; i++) {
-    const number2 = data[i]
-    for (let j = i+1; j < data.length; j++) {
-      const number3 = data[j]
-      if (number1 + number2 + number3  == 2020) {
-        const res: number = number1 * number2 * number3
-        console.log(res)
+function multiplyThreeThatSumTo2020(data: number[]): number | undefined {
+  for (let k = 0; k < data.length; k++) {
+    const number1 = data[k]
+    for (let i = k+1; i < data.length; i++) {
+      const number2 = data[i]
+      for (let j = i+1; j < data.length; j++) {
+        const number3 = data[j]
+        if (number1 + number2 + number3  == 2020) {
+          return number1 * number2 * number3
+        }
       }
     }
   }
 }
+
+const data = readFileSplitByNewLine('/day01/input.txt').map(Number);
+
+const day01part2Result = multiplyThreeThatSumTo2020(data);
+
+console.log('Multiplied three expenses that sum up to 2020: ' + day01part2Result);
